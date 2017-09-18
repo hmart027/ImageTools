00ad330a85fbbadca0a2402709b38dcbec5ae2ab
Harold Martin <harold.martin92@gmail.com>
2017-09-18 16:04:30 -0400

    General Changes

--- src/test/ImgTests.java
+++ src/test/ImgTests.java
@@ -9,14 +9,18 @@ public class ImgTests {
 	public static void main(String[] args){
 		String path = "C:/Users/Public/Pictures/Sample Pictures";
 		path = "C:/Users/Harold/Pictures";
-		byte[][][] img = ImageManipulation.loadImage(path+"/"+"desert.jpg");
-		new IViewer("orig", ImageManipulation.getBufferedImage(img));
-		double[][][] img2 = ITools.getNormColors(ITools.byte2double(img));
-		new IViewer("norm", ImageManipulation.getBufferedImage(ITools.normilizeColor(new double[][][]{img2[0], img2[1], img2[2]})));
-		new IViewer("gray", ImageManipulation.getGrayBufferedImage(ITools.normilize(img2[3])));
-		new IViewer("red", ImageManipulation.getGrayBufferedImage(ITools.normilize(img2[0])));
-		new IViewer("green", ImageManipulation.getGrayBufferedImage(ITools.normilize(img2[1])));
-		new IViewer("blue", ImageManipulation.getGrayBufferedImage(ITools.normilize(img2[2])));
+//		byte[][][] img = ImageManipulation.loadImage(path+"/"+"desert.jpg");
+//		new IViewer("orig", ImageManipulation.getBufferedImage(img));
+//		double[][][] img2 = ITools.getNormColors(ITools.byte2double(img));
+//		new IViewer("norm", ImageManipulation.getBufferedImage(ITools.normilizeColor(new double[][][]{img2[0], img2[1], img2[2]})));
+//		new IViewer("gray", ImageManipulation.getGrayBufferedImage(ITools.normilize(img2[3])));
+//		new IViewer("red", ImageManipulation.getGrayBufferedImage(ITools.normilize(img2[0])));
+//		new IViewer("green", ImageManipulation.getGrayBufferedImage(ITools.normilize(img2[1])));
+//		new IViewer("blue", ImageManipulation.getGrayBufferedImage(ITools.normilize(img2[2])));
+		
+		for(int i=0; i<180; i+=20){
+			new IViewer(i+"",ImageManipulation.getGrayBufferedImage(ITools.normilize(ITools.getEdgeKernel(i))));
+		}
 		
 	}
 
